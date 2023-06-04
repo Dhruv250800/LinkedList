87e687b1959544bdc6e8c864d7627b470c72b10a
DhruvGour <dhruv250800@gmail.com>
2023-06-04 23:31:55 +0530

    [DhruvGour]-adding UC02LinkedList

---
+++ UC02LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class UseCase2LinkedList : LinkedList<int>
+    {
+        public void CreateLinkedList()
+        {
+            AddLast(70);
+            AddLast(30);
+            AddLast(56);
+            Display();  // Output: 70 30 56
+        }
+    }
+}
