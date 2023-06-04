6a632b9e39e716acd8323055f00a7f3e5fe599c7
DhruvGour <dhruv250800@gmail.com>
2023-06-04 23:38:08 +0530

    [DhruvGour]-adding UC04LinkedList

---
+++ UC04LinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class UseCase4LinkedList : LinkedList<int>
+    {
+        public void CreateLinkedList()
+        {
+            AddLast(56);
+            InsertAfter(56, 30);
+            AddLast(70);
+            Display();  // Output: 56 30 70
+        }
+
+        public void InsertAfter(int existingValue, int value)
+        {
+            Node<int> current = head;
+            while (current != null)
+            {
+                if (current.data == existingValue)
+                {
+                    Node<int> newNode = new Node<int>(value);
+                    newNode.next = current.next;
+                    current.next = newNode;
+                    if (current == tail)
+                        tail = newNode;
+                    count++;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+}
