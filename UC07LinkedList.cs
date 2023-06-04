160f2b43c1f886fc20060f3eaac721e9678bd7a9
DhruvGour <dhruv250800@gmail.com>
2023-06-04 23:47:10 +0530

    [DhruvGour]-adding UC07LinkedList

---
+++ UC07LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class UseCase7LinkedList : LinkedList<int>
+    {
+        public void CreateLinkedList()
+        {
+            AddLast(56);
+            AddLast(30);
+            AddLast(70);
+            int searchValue = 30;
+            Node<int> foundNode = null;
+            Node<int> current = head;
+            while (current != null)
+            {
+                if (current.data == searchValue)
+                {
+                    foundNode = current;
+                    break;
+                }
+                current = current.next;
+            }
+            if (foundNode != null)
+                Console.WriteLine("Found: " + foundNode.data);
+            else
+                Console.WriteLine("Not found");
+        }
+    }
+
+}
