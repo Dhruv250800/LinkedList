6b2cdc0104313b90ccb4d22d17eade2339f5ee90
DhruvGour <dhruv250800@gmail.com>
2023-06-04 23:35:12 +0530

    [DhruvGour]-adding UC03LinkedList

---
+++ UC03LinkedList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class UseCase3LinkedList : LinkedList<int>
+    {
+        public void CreateLinkedList()
+        {
+            AddFirst(56);
+            AddLast(30);
+            AddLast(70);
+            Display();  // Output: 56 30 70
+        }
+    }
+}
