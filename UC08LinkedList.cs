2d04044a632f73152e6d0f5eb7abe18956b2a460
DhruvGour <dhruv250800@gmail.com>
2023-06-05 00:11:27 +0530

    [DhruvGour]-adding UC08LinkedList

---
+++ UC08LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class UseCase8LinkedList : LinkedList<int>
+    {
+        public void CreateLinkedList()
+        {
+            AddLast(56);
+            AddLast(30);
+            AddLast(70);
+            InsertAfter(30, 40);
+            Display();  // Output: 56 30 40 70
+        }
+
+        public void InsertAfter(int existingValue, int value)
+        {
+            Node<int> current = head;
+            while (current != null)
+            {
+                if (current.data.Equals(existingValue))
+                {
+                    Node<int> newNode = new Node<int>(value);
+                    newNode.next = current.next;
+                    current.next = newNode;
+                    if (current == tail)
+                        tail = newNode;
+                    count++;
+                    break;
+                }
+                current = current.next;
+            }
+        }
+    }
+
+}
